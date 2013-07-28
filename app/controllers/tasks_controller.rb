ad00b334465deab61cadb3e176f74ef9337bc918
Edmund Dipple <elmundio1987@gmail.com>
2013-07-28 15:28:28 +0100

    initial commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,28 @@
+class TasksController < ApplicationController
+
+def new 
+end
+
+def create
+	@task = Task.new(task_params)
+
+	@task.save
+	redirect_to @task
+	
+	#render text: params[:task].inspect
+end
+
+def show
+  @task = Task.find(params[:id])
+end
+
+def index
+	@tasks = Task.all
+end
+
+private
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kannaban::Application.config.secret_key_base = '58d0b766ad165b6499008e82d46fab2538add642a3836297cbcb89e334ce359281038a0fed759ad42ca75e523bbb4e23a46c0df89aa8aabe3d970a8bff748d09'
